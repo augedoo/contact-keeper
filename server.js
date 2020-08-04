58e5221790cee1cabab07fc5cc56e858ef630447
augedoo <augedoo@gmail.com>
2020-08-04 12:29:10 +0000

    Removed initail route

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-  return res.json({ msg: 'Welcome the ContactKeeper API...' });
-});
-
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
